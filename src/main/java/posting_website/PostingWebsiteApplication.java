97cc82d55757af42a3459f16b2f7984563e3b68f
NadiaAlshariah <nadiakalshariah@gmail.com>
2023-11-24 11:47:52 +0300

    first commit

---
+++ src/main/java/posting_website/PostingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package posting_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostingWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostingWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/posting_website/PostingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package posting_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostingWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
